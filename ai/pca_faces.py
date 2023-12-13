60da662882a2f055e0fffa9089fa29d195cbf55a
podolskiya <145561835+podolskiya@users.noreply.github.com>
2023-12-13 12:24:42 +0100

    Update pca_faces.py

--- ai/pca_faces.py
+++ ai/pca_faces.py
@@ -1,26 +1,30 @@
 import numpy as np
-from sklearn import datasets
+import pandas as pd
+from sklearn.decomposition import PCA
 import codecademylib3
 import matplotlib.pyplot as plt
 
-faces = datasets.fetch_olivetti_faces()['data']
-faces_mean = faces.mean(axis=0)
-faces_std = faces.std(axis=0)
-faces_standardized = (faces - faces_mean) / faces_std
- 
- 
-n_images, n_features = faces_standardized.shape
-side_length = int(np.sqrt(n_features))
-print(f'Number of features(pixels) per image: {n_features}')
-print(f'Square image side length: {side_length}')
- 
- 
-fig = plt.figure(figsize=(10, 8))
 
+faces_standardized = pd.read_csv('./faces_standardized.csv').values
+
+
+pca = PCA(n_components=40) 
+pca.fit(faces_standardized)
+eigenfaces = pca.components_ 
+
+fig = plt.figure(figsize=(10, 8))
+fig.suptitle('Eigenvectors of Images (Eigenfaces)')
 for i in range(15):
     ax = fig.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
-    ax.set_title(f'Image of Face: #{i}')
-    face_image = faces_standardized[i]
-    face_image_reshaped = face_image.reshape(side_length, side_length)
-    ax.imshow(face_image_reshaped, cmap=plt.cm.bone)
+    ax.set_title(f'Eigenface: #{i}')
+    eigenface = eigenfaces[i]
+    eigenface_reshaped = eigenface.reshape(64, 64)
+    ax.imshow(eigenface_reshaped, cmap=plt.cm.bone)
 plt.show()
+
+# pca calculated
+principal_components = pca.transform(faces_standardized) 
+
+# Reconstruction of images in the original size
+faces_reconstructed = pca.inverse_transform(principal_components)
+fig = plt.figure(figsize=(10, 8))
